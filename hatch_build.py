55f436e64b0308e3bf955df07de2c5114a3c3bfb
iwanicki92 <iwanicki92@gmail.com>
2023-10-14 18:48:43 +0200

    Add ngspice license and include it on Windows install

--- hatch_build.py
+++ hatch_build.py
@@ -1,8 +1,11 @@
 import platform
+
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
 
 # Add ngspice.dll when building for Windows
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         if platform.system() == "Windows" and version != "editable":
             build_data['force_include']['lib/ngspice.dll'] = 'phyether/ngspice.dll'
+            build_data['force_include']['lib/LICENSE'] = 'phyether/LICENSE.ngspice'
