134d2dbbbdb05be4f930729d9039ea5774a0182e
iwanicki92 <iwanicki92@gmail.com>
2023-12-14 22:31:35 +0100

    hatch_build.py: fix ngspice license install on windows

--- hatch_build.py
+++ hatch_build.py
@@ -8,4 +8,4 @@ class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         if platform.system() == "Windows" and version != "editable":
             build_data['force_include']['lib/ngspice.dll'] = 'phyether/ngspice.dll'
-            build_data['force_include']['lib/LICENSE'] = 'phyether/LICENSE.ngspice'
+            build_data['force_include']['lib/LICENSE.ngspice'] = 'phyether/LICENSE.ngspice'
