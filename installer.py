02e838ad273cde1fa81b0dd0e2dd791d1a27f02c
iwanicki92 <iwanicki92@gmail.com>
2023-12-16 13:46:55 +0100

    fix windows installer

--- installer.py
+++ installer.py
@@ -4,7 +4,7 @@ import platform
 shared = ("pyinstaller", "src/phyether/main.py", "-n", "phyether", "--noconfirm", "--exclude-module", "PyQt6", "--collect-all", "phyether", "--collect-all", "PySpice", "--collect-data", "galois")
 
 if platform.system() == "Windows":
-    subprocess.call(shared + ("--add-data lib:.", "-w"))
+    subprocess.call(shared + ("--add-data", "lib:.", "-w"))
 elif platform.system() == "Linux":
     subprocess.call(shared)
 else:
